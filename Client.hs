29c17d2e09fcc842193729a8f63b24eff11237e3
韩冬 <handong02@baidu.com>
2015-04-20 18:55:10 +0800

    mass update

--- Client.hs
+++ Client.hs
@@ -2,36 +2,100 @@
 
 import System.Environment
 import System.Exit
+import System.Directory
 import System.IO.Error (catchIOError)
-import Control.Monad.Trans.Resource
+import System.FilePath
 import Control.Monad.IO.Class
 import Control.Monad
-import Data.Conduit.Binary (sinkFile)
-import Network.HTTP.Conduit
-import qualified Data.Conduit as C
+import Control.Concurrent (threadDelay)
+import Network.Http.Client
+import qualified System.FSNotify as FN
+import qualified Data.ByteString as B hiding (pack)
+import qualified Data.ByteString.Char8 as B (pack)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T (decodeUtf8)
+
+import Data.String (fromString)
+import Data.Time.Clock
+import qualified Filesystem.Path as P (FilePath)
+import Filesystem.Path.CurrentOS (encodeString)
+import Network.URI
+import Rainbow
 
 main :: IO ()
 main = getArgs >>= parse
 
-parse ["-h"] = usage >> exit
-parse ["-v"] = version >> exit
-parse (addr:[]) = startMonitor addr
-parse _ = usage >> exit
+parse ["-h"] = usage
+parse ["-v"] = version
+parse (addr:[]) = case parseAddr addr of
+    Just (host, port, path) -> testConnection host port path
+    Nothing                 -> print "URI failed to parse."
+parse _ = usage
 
 usage   = putStrLn "Usage: hsync [-vh] [remote addr]"
 version = putStrLn "hsync 0.1"
-exit    = exitWith ExitSuccess
 
-startMonitor :: String -> IO ()
-startMonitor = \addr -> do
-    initReq <- parseUrl addr
-    let req = initReq { method = "POST" }
-    withManager $ \manager -> do
-        liftIO $ catchIOError
-            (void $ httpLbs req manager) $ 
-            \e -> putStrLn "Can't reach remote service..." >> exit
+parseAddr :: String -> Maybe (Hostname, Port, URI)
+parseAddr addr = do
+    u <- parseURI addr
+    ua <- uriAuthority u
+    rUri <- parseRelativeReference $ uriPath u
+    let port = case uriPort ua of (':':num) -> read num
+                                  _         -> 80
+
+    return ( fromString $ uriRegName ua, port, rUri )
+
+printGreen s = putChunkLn $ ( chunkFromText $ T.pack $ show s ) <> fore green
+
+testConnection :: Hostname -> Port -> URI -> IO ()
+testConnection host port uri = do
+    c <- openConnection host port
+    req <- buildRequest $ http OPTIONS (fromString $ show uri)
+    sendRequest c req emptyBody
+    opt <- receiveResponse c concatHandler
+    if B.take 12 opt == "hsync server"
+        then do
+            cd <- getCurrentDirectory
+            putStr "Start sync service at: " >> printGreen cd
+            putStr "Remote HOST: " >> printGreen host
+            putStr "Remote PORT: " >> printGreen port
+            putStr "Remote PATH: " >> printGreen uri
+
+            startMonitor c uri cd
+        else do
+            putStrLn "hsync server not found" >> closeConnection c
+
+
+startMonitor :: Connection -> URI -> FilePath -> IO ()
+startMonitor c uri cd = do 
+    FN.withManager $ \mgr -> do
+        FN.watchTree mgr "." (const True) (sync c uri cd)
+        -- sleep forever (until interrupted)
+        forever $ threadDelay maxBound
+
+sync :: Connection -> URI -> FilePath -> FN.Event -> IO ()
+sync c uri cd e = case e of
+    FN.Removed  p t  -> go DELETE uri (relativeFilePath p) t
+    FN.Added    p t  -> go PUT    uri (relativeFilePath p) t
+    FN.Modified p t  -> go PUT    uri (relativeFilePath p) t
+    where
+        relativeFilePath p = makeRelative cd $ encodeString p
+        
+        go op uri p t = case parseRelativeReference p of
+            Nothing   -> do
+                putStr $ show t ++ " " ++ show op ++ " failed "
+                printGreen "parse relative path failed"
+            Just relativeURI -> do
+                let fUri = relativeURI `relativeTo` uri
+                putStr $ show t ++ " " ++ show op ++ " at "
+                printGreen fUri
+                let body = case op of DELETE -> emptyBody
+                                      PUT    -> fileBody p
+
+                req <- buildRequest $ http op $ B.pack $ show fUri
+                sendRequest c req body 
 
 
 
-
+        encodeFilePath = encodeFilePath
 
--- Server.hs
+++ Server.hs
@@ -3,17 +3,19 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeOperators #-}
 
+import System.Environment
+import System.Exit
 import Web.Apiary
 import Network.Wai.Handler.Warp
 import Control.Monad.Apiary.Action
 import Control.Monad
 import System.IO.Error (catchIOError)
-import Data.Monoid
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T (decodeUtf8)
 import qualified Data.ByteString.Lazy as B (writeFile, readFile)
 import qualified Data.ByteString as BS (writeFile)
-import qualified Data.ByteString.Char8 as B (unpack)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import System.FilePath
@@ -23,73 +25,95 @@ import Data.Time.Clock
 import Codec.Digest.SHA
 import Codec.Digest.SHA.Misc
 
+main = getArgs >>= parse
+parse ["-h"] = usage >> exit
+parse ["-v"] = version >> exit
+parse (port:[]) = serve $ read port
+parse _ = usage >> exit
+
+usage   = putStrLn "Usage: hsync-server [-vh] [port]"
+version = putStrLn "hsync server 0.1"
+exit    = exitWith ExitSuccess
+
+serve :: Port -> IO ()
+serve p = runApiary (run p) def $ do
+
+    root . method GET . action $ defaultDocumentationAction def 
+            {   documentTitle       = "HSYNC DOC" 
+            ,   documentUseCDN      = False
+            ,   documentDescription = Just $
+                    H.form H.! A.method "POST" H.! A.target "_blank" H.! A.enctype "multipart/form-data"
+                        H.! A.onsubmit "this.action=this.lastChild.value;" $ mconcat
+                    [   "Upload file to path below:"
+                    ,   H.input H.! A.name "files[]" H.! A.multiple "" H.! A.type_ "file"
+                    ,   H.input H.! A.style "width:100%" H.! A.value "home/..."
+                    ]
+            }
+    [capture|/static/api-documentation.js|] . action $ file "static/api-documentation.js" Nothing
+    [capture|/static/api-documentation.css|] . action $ file "static/api-documentation.css" Nothing
+
+
+    [capture|/**path[remote path or folder]|] $ do
+
+        method OPTIONS . document "API for client detecting hsync service" . action $ do
+            dir <- getFilePath
+            succ <- liftIO $ catchIOError
+                (createDirectoryIfMissing True dir >> return True) (const False)
+            when succ $ do
+                logOperation "Sync client detected" dir
+                contentType "text/plain" >> bytes "hsync server 0.1"
+
+        method GET . document "Read file at given path" . action $ do
+            p <- getFilePath
+            logOperation "Read" p
+            devFile p
+
+        method DELETE . document "Remove file at given path" . action $ do
+            p <- getFilePath
+            logOperation "Remove" p
+            performOperation $ removeFile p
+
+        method PUT . document "Write/Create file at given path" . action $ do
+            p <- getFilePath
+            contentType "text/plain"
+            Unknown bs <- getReqBody
+            writePutFile p bs
+
+        method POST . document "Write/Create file to given folder" . action $ do
+            dir <- getFilePath
+            contentType "text/plain"
+            mapM_ (writePostFile dir) =<< getReqBodyFiles
+
+getFilePath :: ActionT exts '["path" ':= [Text]] IO FilePath
 getFilePath = do
     p <- param [key|path|]
     return $ combine "/" (joinPath $ map T.unpack p)
 
-logFileOperation op file = liftIO $ do
+logOperation :: MonadIO m => String -> FilePath -> m ()
+logOperation op file = liftIO $ do
     t <- getCurrentTime
-    putStr $ show t ++ " " ++ op <> " file at "
+    putStr $ show t ++ " " ++ op ++ " at "
     putChunkLn $ (chunkFromText $ T.pack file ) <> fore green
 
-performFileOperation io = liftIO $ catchIOError io $ putStrLn . show
+performOperation :: MonadIO m => IO () -> ActionT exts prms m ()
+performOperation io = liftIO $ catchIOError io $ putStrLn . show
 
+writePostFile :: MonadIO m => FilePath -> File -> ActionT exts prms m ()
 writePostFile dir f = do
-    performFileOperation $ createDirectoryIfMissing True dir
-    let fp = combine dir $ B.unpack $ fileName f
-    logFileOperation "Write" fp
-    performFileOperation $ B.writeFile fp $ fileContent f
+    performOperation $ createDirectoryIfMissing True dir
+    let fp = combine dir $ T.unpack $ T.decodeUtf8 $ fileName f
+    logOperation "Write" fp
+    performOperation $ B.writeFile fp $ fileContent f
     verifyfileSHA fp
 
+writePutFile :: MonadIO m => FilePath -> ByteString -> ActionT exts prms m ()
 writePutFile p bs = do
-    performFileOperation $ createDirectoryIfMissing True $ takeDirectory p
-    logFileOperation "Write" p
-    performFileOperation $ BS.writeFile p $ bs
+    performOperation $ createDirectoryIfMissing True $ takeDirectory p
+    logOperation "Write" p
+    performOperation $ BS.writeFile p $ bs
     verifyfileSHA p
 
+verifyfileSHA :: MonadIO m => FilePath -> ActionT exts prms m ()
 verifyfileSHA f = do
     nf <- liftIO $ B.readFile f
     appendString $ (showBSasHex $ hash SHA512 nf) ++ "\n"
-
-main :: IO ()
-main = runApiary (run 3000) def $ do
-
-        -- add documentation page route.
-        root . action $
-            defaultDocumentationAction def 
-                {   documentTitle       = "HSYNC DOC" 
-                ,   documentUseCDN      = False
-                ,   documentDescription = Just $
-                        H.form H.! A.method "POST" H.! A.target "_blank" H.! A.enctype "multipart/form-data"
-                            H.! A.onsubmit "this.action=this.lastChild.value;" $ mconcat
-                        [   "Upload/update file to path below:"
-                        ,   H.input H.! A.name "files[]" H.! A.multiple "" H.! A.type_ "file"
-                        ,   H.input H.! A.style "width:100%" H.! A.value "home/..."
-                        ]
-                }
-        [capture|/static/api-documentation.js|] . action $ file "static/api-documentation.js" Nothing
-        [capture|/static/api-documentation.css|] . action $ file "static/api-documentation.css" Nothing
-
-        [capture|/**path[file path]|] $ do
-            method GET . document "Read file api" . action $ do
-                p <- getFilePath
-                logFileOperation "Read" p
-                devFile p
-
-            method DELETE . document "Remove file api" . action $ do
-                p <- getFilePath
-                logFileOperation "Remove" p
-                performFileOperation $ removeFile p
-
-            method POST . document "Write/Create file api" . action $ do
-                dir <- getFilePath
-                contentType "text/plain"
-                mapM_ (writePostFile dir) =<< getReqBodyFiles
-
-            method PUT . document "Write/Create file api" . action $ do
-                p <- getFilePath
-                contentType "text/plain"
-                b <- getReqBody
-                case b of
-                    Unknown bs -> writePutFile p bs
-                    _ -> bytes "PUT file in request body..."
